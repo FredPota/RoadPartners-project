c5edcafe9d0e28c1759e30288c7cc9920522327e
Alfred <alfred.delgadoe@hotmail.com>
2026-05-24 21:19:35 -0600

    feat: Se agregan botones para iniciar, cancelar y finalizar viajes en la página de detalles del viaje. Además, se corrigen los placeholders en los campos de origen y destino en la página de inicio.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -257,10 +257,12 @@ function HomePage() {
                                     <PlacesInput
                                         label="Origen"
                                         onPlaceSelected={(data) => setSearchOrigin(data)}
+                                        Placeholder="Ingresa el origen"
                                     />
                                     <PlacesInput
                                         label="Destino"
                                         onPlaceSelected={(data) => setSearchDestiny(data)}
+                                        placeholder="Ingresa el destino"
                                     />
                                 </>
                             ) : (
